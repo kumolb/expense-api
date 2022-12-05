0b5307848c31a8a96896b6e1ef77c1ec9119293c
kumol <kumol@yotech.ltd>
2022-12-05 15:43:08 +0600

    dep

--- models/db.js
+++ models/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://kumol:kumol254@cluster0.tsazd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", (err) => {
+mongoose.connect("process.env.DBURL", (err) => {
     err ? console.log(err) : console.log("connected");
 })
