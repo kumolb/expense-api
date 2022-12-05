2f8f3f370efb501fa81397de4e3abe15c0674100
kumol <kumol@yotech.ltd>
2022-12-05 15:59:28 +0600

    error with environment variable

--- models/db.js
+++ models/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("process.env.DBURL", (err) => {
+mongoose.connect(process.env.DBURL, (err) => {
     err ? console.log(err) : console.log("connected");
 })
